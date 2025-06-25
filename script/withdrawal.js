c9032c65a2291237be05dc4d1aea4d617cf52d85
yohanelly95 <yohan.nelson@gmail.com>
2025-06-25 12:21:42 +0530

    feat: withdrawal proof wip/exploration

--- script/withdrawal.js
+++ script/withdrawal.js
@@ -1,10 +1,10 @@
-import { createProof, ProofType } from '@chainsafe/persistent-merkle-tree';
 import { ssz } from '@lodestar/types';
 
+import { createProof, ProofType } from '@chainsafe/persistent-merkle-tree';
 import { createClient } from './client.js';
 import { toHex, verifyProof } from './utils.js';
 
-const BeaconBlock = ssz.deneb.BeaconBlock;
+const BeaconBlock = ssz.electra.BeaconBlock;
 
 /**
  * @param {string|number} slot
@@ -13,47 +13,39 @@ const BeaconBlock = ssz.deneb.BeaconBlock;
 async function main(slot = 'finalized', withdrawalIndex = 0) {
     const client = await createClient();
 
-    /** @type {import('@lodestar/api').ApiClientResponse} */
-    let r;
-
-    // Requesting the corresponding beacon block to fetch withdrawals.
-    r = await client.beacon.getBlockV2(slot);
-    if (!r.ok) {
-        throw r.error;
+    // Get the beacon block for the slot from the beacon node.
+    console.log(`Fetching block for slot ${slot} from the beacon node`);
+    const blockRes = await client.beacon.getBlockV2({ blockId: slot });
+    if (!blockRes.ok) {
+        throw blockRes.error;
     }
-
-    const blockView = BeaconBlock.toView(r.response.data.message);
+    const blockView = BeaconBlock.toView(blockRes.value().message);
     const blockRoot = blockView.hashTreeRoot();
-
+    console.log(`Beacon block root: ${toHex(blockRoot)}`);
     const nav = blockView.type.getPathInfo(['body', 'executionPayload', 'withdrawals', withdrawalIndex]);
     const p = createProof(blockView.node, { type: ProofType.single, gindex: nav.gindex });
 
-    // Sanity check: verify gIndex and proof match.
+    // // Sanity check: verify gIndex and proof match.
     verifyProof(blockRoot, nav.gindex, p.witnesses, p.leaf);
 
     // Since EIP-4788 stores parentRoot, we have to find the descendant block of
     // the block from the state.
-    r = await client.beacon.getBlockHeaders({ parentRoot: blockRoot });
-    if (!r.ok) {
-        throw r.error;
+    const blockHeader = await client.beacon.getBlockHeaders({slot, parentRoot: blockRoot.toString() });
+    if (!blockHeader.ok) {
+        throw blockHeader.error;
     }
-
-    /** @type {import('@lodestar/types/lib/phase0/types.js').SignedBeaconBlockHeader} */
-    const nextBlock = r.response.data[0]?.header;
-    if (!nextBlock) {
-        throw new Error('No block to fetch timestamp from');
-    }
-
+    console.log(blockHeader.value());
+    
     // Create output for the Verifier contract.
     return {
         blockRoot: toHex(blockRoot),
         proof: p.witnesses.map(toHex),
         withdrawal: nav.type.toJson(blockView.body.executionPayload.withdrawals.get(withdrawalIndex)),
         withdrawalIndex: withdrawalIndex,
-        ts: client.slotToTS(nextBlock.message.slot),
+        ts: client.slotToTS(slot + 1),
         gI: nav.gindex,
     };
 }
 
-main(7424512, 9).then(console.log).catch(console.error);
+main(42600, 0).then(console.log).catch(console.error);
 //            ^_ withdrawal index in withdrawals array
