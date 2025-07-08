530e92bdb3eb814b40d9a1e2afe4744664ee07b2
yohanelly95 <yohan.nelson@gmail.com>
2025-07-08 15:33:26 +0530

    chore: cleanup

--- script/DeployBalanceContainerRootVerifier.s.sol
+++ script/DeployBalanceContainerRootVerifier.s.sol
@@ -7,9 +7,6 @@ import {BalanceContainerRootVerifier} from '../src/BalanceContainerRootVerifier.
 
 contract DeployBalanceContainerRootVerifier is Script {
     function run() external returns (BalanceContainerRootVerifier deployed) {
-        // Generalized index for balance container in beacon state
-        // This value is calculated from the SSZ tree structure
-        // uint256 gIndexBalanceContainer = 549755813890;
 
         vm.startBroadcast();
         deployed = new BalanceContainerRootVerifier();
