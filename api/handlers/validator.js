907e583956212ec60a2feb88eeb89a24518a1817
yohanelly95 <yohan.nelson@gmail.com>
2025-07-21 13:55:59 +0530

    chore: minor cleanup

--- api/handlers/validator.js
+++ api/handlers/validator.js
@@ -2,30 +2,6 @@ import {
     generateMultipleValidatorProofsWrapper
 } from "../../script/api-validator.js";
 
-// export const getValidatorProof = async (req, res) => {
-//   try {
-//     const { slot, validatorIndex } = req.params;
-
-//     const slotNumber = Number(slot);
-//     const validatorIndexNumber = Number(validatorIndex);
-
-//     if (isNaN(slotNumber) || isNaN(validatorIndexNumber)) {
-//       return res.status(400).json({ error: "Invalid slot or validator index" });
-//     }
-
-//     const proof = await generateValidatorProofWrapper(
-//       slotNumber,
-//       validatorIndexNumber
-//     );
-
-//     // The wrapper already returns JSON string, parse it for response
-//     res.json(proof);
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: "Internal server error" });
-//   }
-// };
-
 export const getMultipleValidatorProofs = async (req, res) => {
   try {
     const { slot } = req.params;
